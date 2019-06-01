85aa03a7a1ed37bc841bb1dbf5ff7c1ed246cf1e
jstran9 <wptran58@gmail.com>
2019-05-31 18:18:34 -0700

    implementing get all books.

---
+++ src/main/java/tran/todd/bookapi/api/v1/dao/BookDaoImpl.java
@@ -0,0 +1,39 @@
+package tran.todd.bookapi.api.v1.dao;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import tran.todd.bookapi.api.v1.model.Book;
+
+import java.util.List;
+
+public class BookDaoImpl implements BookDao {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public Long save(Book book) {
+        return null;
+    }
+
+    @Override
+    public Book get(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Book> list() {
+        return sessionFactory.getCurrentSession().createQuery("from Book").list();
+
+    }
+
+    @Override
+    public void update(Long id, Book book) {
+
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
