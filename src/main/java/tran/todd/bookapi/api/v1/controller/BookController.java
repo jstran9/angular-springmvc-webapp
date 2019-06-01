808d2650143c5e8887fdc6c3074e74626e1f9450
jstran9 <wptran58@gmail.com>
2019-05-31 18:40:20 -0700

    handling request to save a book.

--- src/main/java/tran/todd/bookapi/api/v1/controller/BookController.java
+++ src/main/java/tran/todd/bookapi/api/v1/controller/BookController.java
@@ -2,21 +2,21 @@ package tran.todd.bookapi.api.v1.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tran.todd.bookapi.api.v1.model.Book;
 import tran.todd.bookapi.api.v1.service.BookService;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/")
 public class BookController {
 
     @Autowired
     private BookService bookService;
 
     // Get all the books
-    @GetMapping("/api/books")
+    @GetMapping("books")
     public ResponseEntity<List<Book>> list() {
         /*
          * note: the ResponseEntity is not our return type. Instead of we will create a custom class and add
@@ -24,5 +24,12 @@ public class BookController {
          */
         List<Book> books = bookService.list();
         return ResponseEntity.ok().body(books);
+    }
+
+    // Save the book
+    @PostMapping("book")
+    public ResponseEntity<?> save(@RequestBody Book book) {
+        Long bookId = bookService.save(book);
+        return ResponseEntity.ok().body("Book created with id: " + bookId);
     }
 }
